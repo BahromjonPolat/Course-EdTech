c7304200f7b86df82e15950d7a9d6895463bb5ce
Zuhriddin91 <turoni1991@gmail.com>
2021-11-26 15:12:56 +0500

    test

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -10,6 +10,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:  Center(
+        child:  Text("Hello"),
+      ),
+    );
   }
 }
