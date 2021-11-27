889567778b680b39c7e17c5f14430cfaed5c7b75
BahromjonPolat <itjunior.uz@gmail.com>
2021-11-27 10:36:52 +0500

    Fontlar qo'shildi, CustomElevatedButton tayyorlandi

--- lib/components/importing_packages.dart
+++ lib/components/importing_packages.dart
@@ -3,3 +3,5 @@ export 'package:course/components/size_config.dart';
 export 'package:course/components/const.dart';
 export 'package:firebase_core/firebase_core.dart';
 export 'package:flutter/services.dart';
+export 'package:course/widgets/buttons/elevated_button.dart';
+
