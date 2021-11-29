4732aeb67f71dea24159181a412fa653932cb0d5
Abdullohali <muhammadfoziljonov019@gmail.com>
2021-11-29 16:32:55 +0500

    New file

---
+++ lib/screens/auth/profile_page.dart
@@ -0,0 +1,17 @@
+import '../../components/importing_packages.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({ Key? key }) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
